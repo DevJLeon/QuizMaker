2bb4452750030f28b6e2a74d9917162ca115c19b
Joshua <celisjoshleon@gmail.com>
2023-06-28 12:37:12 -0500

    second commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,22 @@
 const $form = document.querySelector(".quiz-form");
 const $quiz = document.querySelector(".quiz");
+const $radioBtns = document.querySelectorAll("input['']")
+
 
 var questions = [];
 
 $form.addEventListener("submit", (a)=>{
   a.preventDefault();
-  /* const firstOption = $form.opt1.value;
+  const firstOption = $form.opt1.value;
   const secOption = $form.opt2.value;
   const thirdOption = $form.opt3.value;
+
+  console.log(firstOption);
+  console.log(secOption);
+  console.log(thirdOption);
+
   //const rightAns = $form.opt1.value;
-  
+  /*
   let question = {
     first:firstOption,
     second:secOption,
@@ -24,3 +31,8 @@ $form.addEventListener("submit", (a)=>{
   questions.push(question); */
   $form.reset();
 })
+
+let findSelected = () =>{
+  let $selected = document.querySelector("input[name='ans']:checked");
+  console.log($selected)
+};
